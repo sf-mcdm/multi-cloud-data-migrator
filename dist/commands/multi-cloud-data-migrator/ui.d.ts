afa6557278512cd6651a6b365eadffb02b4a2f85
Your Name <you@example.com>
2025-10-21 00:00:20 +0200

    chore: release v1.258.39

--- dist/commands/multi-cloud-data-migrator/ui.d.ts
+++ dist/commands/multi-cloud-data-migrator/ui.d.ts
@@ -1,5 +1,6 @@
-import { Command } from "@oclif/core";
+import { Command, Config } from "@oclif/core";
 export default class Ui extends Command {
     static summary: string;
+    constructor(argv: string[], config: Config);
     run(): Promise<void>;
 }
