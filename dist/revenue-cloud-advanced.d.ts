281914c7e2d8a4d2af8317dc4c8d9eaef3fa5572
Your Name <you@example.com>
2025-10-21 00:56:48 +0200

    chore: release v1.258.47

--- dist/revenue-cloud-advanced.d.ts
+++
@@ -1,3 +0,0 @@
-import type { ObjectCfg } from "../lib/types.js";
-declare const fullConfig: ObjectCfg[];
-export default fullConfig;
--- dist/ui.d.ts
+++
@@ -1,5 +0,0 @@
-import { Command } from "@oclif/core";
-export default class Ui extends Command {
-    static summary: string;
-    run(): Promise<void>;
-}
